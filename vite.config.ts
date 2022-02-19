dbc992465b6cce8a5232a23956cd15f79a1bf589
芊野 <free.nan.sky@gmail.com>
2022-02-19 22:39:27 +0800

    init

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+    root: resolve(__dirname, './examples'),
+    build: {
+        rollupOptions: {
+            input: {
+                index: resolve(__dirname, './examples/index.html'),
+                child: resolve(__dirname, './examples/child.html')
+            }
+        }
+    }
+})
